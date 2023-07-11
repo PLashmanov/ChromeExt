ca68f3a8e6b8c85a5061f973e746b578941a2097
Pavel Lashmanov <plashmanov@gmail.com>
2023-07-11 11:43:16 -0400

    Final
    Chrome extension to save url's

--- index.js
+++ index.js
@@ -2,22 +2,23 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-    
-})
 
-function renderLeads() {
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+function render(leads) {
     let listItems = ""
-    for (i = 0 ; i < myLeads.length; i++) {
+    for (i = 0 ; i < leads.length; i++) {
         //listItems += "<li><a href =' " + myLeads[i] + "' target = '_blank'>" + myLeads[i] + "</a></li>"
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
@@ -26,3 +27,28 @@ function renderLeads() {
 
     ulEl.innerHTML = listItems
 }
+tabBtn.addEventListener("click", function(){
+     
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    
+    })
+    
+})
+deleteBtn.addEventListener("dblclick", function(){
+    myLeads = []
+    localStorage.clear()
+    render(myLeads)
+    
+})
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+    
+})
+
